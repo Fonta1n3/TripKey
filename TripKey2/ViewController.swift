22544d10cfd6dcff4171d474f7da51bc1e27e097
Peter <peter@hp-print-69.local>
2016-08-21 01:21:14 +0100

    Initial Commit

---
+++ TripKey2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripKey2
+//
+//  Created by Peter on 8/21/16.
+//  Copyright © 2016 Fontaine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
